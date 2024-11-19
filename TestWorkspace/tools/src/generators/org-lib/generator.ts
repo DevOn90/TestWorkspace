71b93c92d537f78bf842361d66a098b7d0b50b97
DevOn90 <jansvatos90@gmail.com>
2024-11-19 17:40:43 +0100

    generator update

--- TestWorkspace/tools/src/generators/org-lib/generator.ts
+++ TestWorkspace/tools/src/generators/org-lib/generator.ts
@@ -6,12 +6,43 @@ import {
 } from '@nx/devkit';
 import * as path from 'path';
 import { OrgLibGeneratorSchema } from './schema';
+import { writeFile } from 'fs/promises';
 
 export async function orgLibGenerator(
   tree: Tree,
   options: OrgLibGeneratorSchema
 ) {
   const projectRoot = `libs/${options.name}`;
+  
+  // const projects = getProjects(tree);
+  // projects.forEach(element => {
+  //   console.log(element)
+  // });
+
+  // Fetch remote configuration to json object
+  
+  
+  async function getLibConfig() {
+    
+    const libConfigUrl:string = 'http://localhost:80/lib-config';
+    const filesPath = path.join(__dirname,"files");
+    const htmlPath = `${filesPath}/src/lib/__name__/__name__.component.html.template`
+
+    try {
+      const response = await fetch(libConfigUrl);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+      }
+
+      const content = await response.json();
+      await writeFile(`${htmlPath}`,content['componentHtml']);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+ 
+  await getLibConfig()
+ 
   addProjectConfiguration(tree, options.name, {
     root: projectRoot,
     projectType: 'library',
@@ -19,7 +50,11 @@ export async function orgLibGenerator(
     targets: {},
   });
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+
   await formatFiles(tree);
+  
+  const newRead = tree.read(`${projectRoot}/src/lib/${options.name}/${options.name}.component.html`).toString();
+  console.log(`New read: ${newRead}`)
 }
 
 export default orgLibGenerator;
--- TestWorkspace/tools/src/generators/org-lib/schema.d.ts
+++ TestWorkspace/tools/src/generators/org-lib/schema.d.ts
@@ -1,3 +1,4 @@
 export interface OrgLibGeneratorSchema {
   name: string;
+  directory: string;
 }
